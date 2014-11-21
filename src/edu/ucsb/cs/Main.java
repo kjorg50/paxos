671058e442172be04c4d5d6e5f3c6aa9310316ae
Kyle Jorgensen <kylewjorgensen@gmail.com>
2014-11-20 21:12:49 -0800

    small update

--- src/edu/ucsb/cs/Main.java
+++ src/edu/ucsb/cs/Main.java
@@ -2,6 +2,9 @@ package edu.ucsb.cs;
 
 import java.util.Scanner;
 
+/**
+ * Main class of our Paxos implementation. Handles the command line interaction with the user.
+ */
 public class Main {
 
     public static final int DEPOSIT = 1;
@@ -9,7 +12,6 @@ public class Main {
     public static final int BALANCE = 3;
     public static final int FAIL = 4;
     public static final int UNFAIL = 5;
-
 
     public static void main(String[] args) {
 
@@ -26,25 +28,35 @@ public class Main {
                         System.out.println("Type amount to deposit:");
                         if (sc.hasNextDouble()) {
                             Double amount = sc.nextDouble();
-                            System.out.println("Depositing " +  amount);
+                            System.out.println("Depositing " + amount);
                             // TODO
                             System.out.println(amount + " deposited");
-                        }else {
+                        } else {
                             System.out.println("Please behave.");
                             sc.next();
                         }
                         break;
                     case WITHDRAW:
-                        System.out.println("Action not supported");
+                        System.out.println("Type amount to withdraw:");
+                        if (sc.hasNextDouble()) {
+                            Double amount = sc.nextDouble();
+                            System.out.println("Withdrawing " + amount);
+                            // TODO
+                            System.out.println(amount + " withdrawn");
+                        } else {
+                            System.out.println("Please behave.");
+                            sc.next();
+                        }
                         break;
                     case BALANCE:
-                        System.out.println("Action not supported");
+                        System.out.println("Your account balance is $<amount>");
+                        // TODO check balance
                         break;
                     case FAIL:
-                        System.out.println("Action not supported");
+                        System.out.println("Failure ON");
                         break;
                     case UNFAIL:
-                        System.out.println("Action not supported");
+                        System.out.println("Failure OFF");
                         break;
                     default:
                         System.out.println("Action not supported");
