32420ef604afeead8be8009c96f9ba34e6bd46d8
Nevena Golubovic <nevena.golubovic@gmail.com>
2014-12-10 00:57:03 -0800

    update ThriftProposalID

--- Ballot.thrift
+++ Ballot.thrift
@@ -2,21 +2,21 @@ namespace java edu.ucsb.cs.thrift
 
 typedef i64 int
 
-struct ProposalID{
+struct ThriftProposalID{
     1:int ballotNumber,
     2:string uid
 }
 
 service Ballot {
 
-    /** ProposalID == (int ballotNumber, string uid) **/
+    /** ThriftProposalID == (int ballotNumber, string uid) **/
 
 
     /* Prepare
      * 1. my node ID
      * 2. the Proposal
      */
-    oneway void prepare(1:string myId, 2:ProposalID propID)
+    oneway void prepare(1:string myId, 2:ThriftProposalID propID)
 
     /* Promise
      * 1. my node ID
@@ -24,25 +24,25 @@ service Ballot {
      * 3. the previous Proposal
      * 4. promised value
      */
-    oneway void promise(1:string myId, 2:ProposalID propID, 3:ProposalID prevPropId, 4:int acceptedValue)
+    oneway void promise(1:string myId, 2:ThriftProposalID propID, 3:ThriftProposalID prevPropId, 4:int acceptedValue)
 
     /* Accept
      *
      */
-    oneway void accept(1:string myId, 2:ProposalID propID, 3:int acceptedValue)
+    oneway void accept(1:string myId, 2:ThriftProposalID propID, 3:int acceptedValue)
 
     /* Accepted
      *
      */
-    oneway void accepted(1:string myId, 2:ProposalID propID, 3:int acceptedValue)
+    oneway void accepted(1:string myId, 2:ThriftProposalID propID, 3:int acceptedValue)
 
 	// if accepted received from majority => onResolution
-    oneway void decide(1:ProposalID propID, 2:int value)
+    oneway void decide(1:ThriftProposalID propID, 2:int value)
 
-    oneway void prepareNACK(1:string myId, 2:ProposalID propID, 3:ProposalID promisedID)
-    oneway void acceptNACK(1:string myId, 2:ProposalID propID, 3:ProposalID promisedID)
+    oneway void prepareNACK(1:string myId, 2:ThriftProposalID propID, 3:ThriftProposalID promisedID)
+    oneway void acceptNACK(1:string myId, 2:ThriftProposalID propID, 3:ThriftProposalID promisedID)
 
-    oneway void heartbeat(1:string myId, 2:ProposalID leaderPropID)
+    oneway void heartbeat(1:string myId, 2:ThriftProposalID leaderPropID)
 
     list<int> update(1:int lastAcceptedBallot)
 }
