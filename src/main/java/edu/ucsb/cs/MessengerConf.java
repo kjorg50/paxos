c9f37d243607256bae5e7620c2ccb26919264e8e
Nevena Golubovic <nevena.golubovic@gmail.com>
2014-12-11 16:36:39 -0800

    adding confs for aws 3 nodes

--- src/main/java/edu/ucsb/cs/MessengerConf.java
+++ src/main/java/edu/ucsb/cs/MessengerConf.java
@@ -11,11 +11,11 @@ public class MessengerConf {
     public Map<Integer, Messenger> messengerConfigurations = new HashMap<Integer, Messenger>();
 
     public MessengerConf() {
-        messengerConfigurations.put(0, new Messenger("localhost", 9090, "us-west"));
-        messengerConfigurations.put(1, new Messenger("localhost", 9091, "us-east"));
-//        messengerConfigurations.put(0, new Messenger("54.183.94.22", 9090, "us-west"));
-//        messengerConfigurations.put(1, new Messenger("54.174.254.189", 9090, "us-east"));
-//        messengerConfigurations.put(2, new Messenger("54.154.90.72", 9090, "europe"));
+//        messengerConfigurations.put(0, new Messenger("localhost", 9090, "us-west"));
+//        messengerConfigurations.put(1, new Messenger("localhost", 9091, "us-east"));
+        messengerConfigurations.put(0, new Messenger("54.183.94.22", 9090, "us-west"));
+        messengerConfigurations.put(1, new Messenger("54.174.254.189", 9090, "us-east"));
+        messengerConfigurations.put(2, new Messenger("54.154.90.72", 9090, "europe"));
 //        messengerConfigurations.put(3, new Messenger("54.169.74.145", 9090, "singapore"));
 //        messengerConfigurations.put(4, new Messenger("54.94.153.129", 9090, "south"));
     }
